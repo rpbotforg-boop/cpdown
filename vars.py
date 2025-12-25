507dc6cff4bd1897ae21bf92a6751763402b2f94
rpbotforg-boop <rpbotforg@gmail.com>
2025-12-26 00:55:09 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,9 @@
 api_id = 22447622 #change_with_your_Api_id.
 api_hash = "543b62d58d3e723e766ba57a984ab65d" #change_with_your_Api_hash.
-bot_token = "7992835160:AAHNoOV1vBG4uxqd3bpPXOiLgmvDoCtus7s" #change_with_your_bot_token.
+bot_token = "8387440827:AAHTwkJTiLb5Wc-Rru407_0d_2UsaYazy4Y" #change_with_your_bot_token.
 auth_users = [777756062] #change_with_your_user_id.
 sudo_user = 777756062 #change_with_your_user_id.
-log_channel = -1002655703820 #change_with_your_channel_id.
-txt_channel = -1002655703820 #change_with_your_channel_id.
+log_channel = -1003270198972 #change_with_your_channel_id.
+txt_channel = -1003270198972 #change_with_your_channel_id.
+
 
